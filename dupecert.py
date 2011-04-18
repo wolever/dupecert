073888ab1d6ec03e43ae22ff6494fae48e603c24
David Wolever <david@wolever.net>
2011-04-18 00:34:26 -0400

    Copy 'subjectAltName'

--- dupecert.py
+++ dupecert.py
@@ -6,12 +6,22 @@ PEM = crypto.FILETYPE_PEM
 
 # see: http://docs.ganeti.org/ganeti/master/html/design-x509-ca.html
 
+def get_extension(cert, short_name, default=None):
+    for i in range(cert.get_extension_count()):
+        extension = cert.get_extension(i)
+        if extension.get_short_name() == short_name:
+            return extension
+    return default
+
 def dupe(cert):
     dst = crypto.X509()
     dst.set_subject(cert.get_subject())
     dst.set_serial_number(1337)
     dst.gmtime_adj_notBefore(0)
     dst.gmtime_adj_notAfter(60*60*24*360)
+    alt_name = get_extension(cert, "subjectAltName")
+    if alt_name is not None:
+        dst.add_extensions([ alt_name ])
 
     key = crypto.PKey()
     key.generate_key(crypto.TYPE_RSA, 1024)
